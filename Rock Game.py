0ad4fa69a471f5a02fc904674addff01245411b6
voroninPA <114736747+voroninPA@users.noreply.github.com>
2022-09-30 14:01:35 +0300

    Create Rock Game.py
    New Game

---
+++ Rock Game.py
@@ -0,0 +1,15 @@
+import random
+numb=random.randint(4,30)
+print("Hi, its rocks game")
+while numb>1:
+    print("number of rocks: ", numb)
+    i=int(input("what number of rocks you want to take:"))
+    numb=numb-i
+    print("number of rocks: ", numb)
+    comp=random.randint(1,3)
+    print("computer take ",comp,"rocks")
+    numb=numb-comp
+if(numb==1):
+    print("you lose")
+else:
+    print("you win")
